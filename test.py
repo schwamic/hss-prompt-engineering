82f731d8297c32820dec4de515a96b9e710543e5
schwamic <schwarz.michael@posteo.net>
2023-12-19 16:23:43 +0100

    chore: init

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-message = "hello world"
+# Run marked code lines with Shift+Enter to run as jupyter notebook.
+# Therefore not the whole script is getting executed and variables are saved in a session.
 
+message = "hello world"
+print(message)
 print(message)
